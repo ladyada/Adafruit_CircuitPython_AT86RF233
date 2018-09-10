d1a09aab668f623ac9115f0bfb7436a5f857f79e
ladyada <limor@ladyada.net>
2018-09-09 22:22:42 -0400

    first init

--- adafruit_at86rf233.py
+++ adafruit_at86rf233.py
@@ -40,7 +40,53 @@ Implementation Notes
 * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
-# imports
+import time
+from micropython import const
+from digitalio import Direction, DigitalInOut
+import adafruit_bus_device.spi_device as spi_device
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_AT86RF233.git"
+
+
+#pylint: disable=bad-whitespace
+# Register and other constant values:
+_READ_REG        = const(0x80)
+_REG_PART_NUM    = const(0x1C)
+_REG_VERSION_NUM = const(0x1D)
+#pylint: enable=bad-whitespace
+
+class AT86RF233:
+    """Driver for the AT86RF233 radio chipset."""
+
+    # Class-level buffer for reading and writing data with the sensor.
+    # This reduces memory allocations but means the code is not re-entrant or
+    # thread safe!
+    _BUFFER = bytearray(32)
+
+    def __init__(self, spi, cs, *, sleep=None, reset=None):
+        self._spi = spi_device.SPIDevice(spi, cs)
+        if sleep:
+            sleep.direction = Direction.OUTPUT
+            sleep.value = False  # out of sleep mode
+        if reset:
+            reset.direction = Direction.OUTPUT
+            reset.value = False
+            time.sleep(0.01)
+            reset.value = True
+        time.sleep(0.01)
+        part_num = self._read_reg(_REG_PART_NUM)
+        if part_num != 0x0b:
+            raise RuntimeError("Part #0x%x wrong" % part_num)
+        vers = self._read_reg(_REG_VERSION_NUM)
+        if vers != 2:
+            raise RuntimeError("Version #%d wrong" % vers)
+        
+
+    def _read_reg(self, reg_addr):
+        # address is only 5 bits!
+        self._BUFFER[0] = _READ_REG | (reg_addr & 0x3F)
+        with self._spi as spi:
+            spi.write(self._BUFFER, end=1)
+            spi.readinto(self._BUFFER, end=1)
+        return self._BUFFER[0]
