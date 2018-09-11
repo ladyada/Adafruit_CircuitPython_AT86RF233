7581d826a2978ee1001a56de937daf1f8e48029f
ladyada <limor@ladyada.net>
2018-09-10 22:01:50 -0400

    somewhat wiresharky

--- examples/sniffer.py
+++ examples/sniffer.py
@@ -1,4 +1,5 @@
 import gc
+import sys
 import time
 import board
 import busio
@@ -13,15 +14,19 @@ reset_pin = DigitalInOut(board.RESETN)
 
 radio = adafruit_at86rf233.AT86RF233(spi_bus, cs_pin, sleep=sleep_pin, reset=reset_pin)
 
+DEBUG = True
+
 radio.channel = 25
-print("Radio channel ", radio.channel)
+if DEBUG:
+    print("Radio channel ", radio.channel)
 
 radio.short_addr = 0
-print("Short address: ", hex(radio.short_addr))
 radio.pan_addr = 0
-print("PAN address: ", hex(radio.pan_addr))
 radio.ieee_addr = [0, 0, 0, 0, 0, 0, 0, 0]
-print("IEEE address: ", [hex(i) for i in radio.ieee_addr])
+if DEBUG:
+    print("Short address: ", hex(radio.short_addr))
+    print("PAN address: ", hex(radio.pan_addr))
+    print("IEEE address: ", [hex(i) for i in radio.ieee_addr])
 
 radio._write_reg(0x17, [0B00000010])  # AACK_PROM_MODE - promisc mode
 radio._write_reg(0x2E, [0B00010000])  # AACK_DIS_ACK - dont send ack frames ever
@@ -32,15 +37,21 @@ radio._write_reg(0x03, [trx_ctrl0])
 led = DigitalInOut(board.D13)
 led.direction = Direction.OUTPUT
 
-print("Free memory: ", gc.mem_free())
+if DEBUG:
+    print("Free memory: ", gc.mem_free())
 
+while input("Send START command: ") != "START":
+    pass
+    
+print("SNIF", end="")
 stamp = time.monotonic()
 while True:
     stat = radio.status
     irq = radio.irq
     status = adafruit_at86rf233.RTX_STATE[stat]
     if time.monotonic() - stamp > 3: # every n seconds, print status
-        print("@", time.monotonic(), " -- status: ", hex(stat), status, "IRQ: ", hex(irq))
+        if DEBUG:
+            print("@", time.monotonic(), " -- status: ", hex(stat), status, "IRQ: ", hex(irq))
         stamp = time.monotonic()
 
     if status == 'P_ON':
@@ -59,5 +70,9 @@ while True:
         #print("\tIRQ RTX END")
         led.value = True
         frame = radio.read_frame()
-        print("Frame size ", len(frame), ":", [hex(i) for i in frame])
+        if DEBUG:
+            print("Frame size ", len(frame), ":", [hex(i) for i in frame])
+        else:
+            x = sys.stdout.write(bytes(len(frame)))
+            x = sys.stdout.write(frame)
         led.value = False
